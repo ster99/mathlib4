7db0be9b7886a647766eb264c1779df01e0b0391
Stefano Rocca <stefano.rocca.99@gmail.com>
2025-11-07 12:38:03 +0100

    feat(MeasureThory/FoelnerFilter.lean): Foelner filters
    -Definition of Folener filters, proof that if a measure space with a
    G-action has a Foelner filter then there is a left-invariant finitely additive
    probability measure on it (amenability).
    -Definition of maximal Foelner filter and proof that if the maximal
    Foelner filter is nontrivial, then the amenability condition above
    holds.

--- Mathlib.lean
+++ Mathlib.lean
@@ -4662,6 +4662,7 @@ import Mathlib.MeasureTheory.Covering.LiminfLimsup
 import Mathlib.MeasureTheory.Covering.OneDim
 import Mathlib.MeasureTheory.Covering.Vitali
 import Mathlib.MeasureTheory.Covering.VitaliFamily
+import Mathlib.MeasureTheory.FoelnerFilter
 import Mathlib.MeasureTheory.Function.AEEqFun
 import Mathlib.MeasureTheory.Function.AEEqFun.DomAct
 import Mathlib.MeasureTheory.Function.AEEqOfIntegral
